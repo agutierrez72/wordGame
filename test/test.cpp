f081c5eedc0a07573d218ba058767cf5ac05d93a
agutierrez72 <agutierrez72@ucmerced.edu>
2020-03-06 15:05:22 -0800

    test checkWord function of Game class

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,40 @@
 #include <igloo/igloo.h>
 
+#include <Game.h>
+
 using namespace igloo;
+
+Context(CheckWord){
+	Game game;
+	ucm::json ans;
+	Spec(CheckFAisInvalid){
+		ans["word"] = "FA ";
+		ans["valid"] = false;
+		Assert::That(game.checkWord("FA "), Equals(ans));
+	};
+	Spec(CheckFACisInvalid){
+		ans["word"] = "FAC ";
+		ans["valid"] = false;
+		Assert::That(game.checkWord("FAC "), Equals(ans));
+	};
+	Spec(CheckTIisInvalid){
+		ans["word"] = "TI";
+		ans["valid"] = false;
+		Assert::That(game.checkWord("TI"), Equals(ans));
+	};
+	Spec(ChecktiIsInvalid){
+		ans["word"] = "ti";
+		ans["valid"] = false;
+		Assert::That(game.checkWord("ti"), Equals(ans));
+	};
+	Spec(CheckAdditionIsValid){
+		ans["word"] = "addition";
+		ans["valid"] = false;
+		Assert::That(game.checkWord("addition"), Equals(ans));
+	};
+
+};
+
 
 int main() {
 	// Run all the tests defined above
