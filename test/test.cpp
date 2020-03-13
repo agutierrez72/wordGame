5ce28b8b03cb59be43158cd937f6e678374f7d67
agutierrez72 <agutierrez72@ucmerced.edu>
2020-03-13 14:08:51 -0700

    removed tests for permutation function

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 #include <igloo/igloo.h>
-#include <string>
 #include <Game.h>
+#include <string>
 #include <algorithm>
 //#include <iterator>
 
@@ -37,33 +37,6 @@ Context(CheckWord){
 
 };
 
-// Test to check if one string is a subset of another
-// Plan to use to find the solution set for 9 letter words
-// Does not work for out of order solutions such as ON -> NO
-// Context(IsPermutation){
-// 	std::string par = "ADDITION";
-// 	std::string sub1 = "ADDIT";
-// 	std::string sub2 = "ION";
-// 	std::string sub3 = "NO";
-
-	
-// 	Spec(addIsPAddition){
-// 		std::sort(par.begin(), par.end());
-// 		std::sort(sub1.begin(), sub1.end());
-// 		Assert::That(std::is_permutation(sub1.begin(), sub1.end(), par.begin()));
-// 	};
-// 	Spec(ionIsPAddition){
-// 		std::sort(par.begin(), par.end());
-// 		std::sort(sub2.begin(), sub2.end());
-// 		Assert::That(boost::algorithm::contains(par, sub2));
-// 	};
-// 	Spec(noIsPAddition){
-// 		std::sort(par.begin(), par.end());
-// 		std::sort(sub3.begin(), sub3.end());
-// 		Assert::That(boost::algorithm::contains(par, sub3));
-// 	};
-// };
-
 Context(IsIntersection){
 	std::string par = "MISSION";
 	std::string sub1 = "NOI";
@@ -76,7 +49,6 @@ Context(IsIntersection){
 		Assert::That(ret, Equals(sol));
 	};
 };
-
 
 int main() {
 	// Run all the tests defined above
